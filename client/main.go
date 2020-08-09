a58c4c38a0cc531fffe6f5082925c9af470a27ce
inggit-prakasa <inggit.prakasa123@gmail.com>
2020-08-09 13:58:06 +0700

    CRUD EMP Server

--- client/main.go
+++ client/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	employee "github.com/inggit_prakasa/HRD/employee"
+	"google.golang.org/grpc"
+	"log"
 )
 
 const (
 	address = "localhost:8080"
 )
+
+var emp *employee.Employee
 
 func main() {
 
@@ -15,17 +22,23 @@ func main() {
 	fmt.Println("----------------------------------------------------------------")
 	var a int
 	fmt.Println("<< 1. Login STAFF >>")
+	// Username :
+	// Password :
 	fmt.Println("<< 2. Login HRD   >>")
+	// Username :
+	// Password :
 	fmt.Println("<< 3. Exit       >>")
-	fmt.Print("Input Login sesuai yang Anda inginkan: ")
-	fmt.Scanln(&a)
+	//IF Role staff -> MENUSTAFF
+	//IF Role hrd -> MENUHRD
 	switch a {
 	case 1:
 		fmt.Println("--------------------------------------------------")
 		fmt.Println("               MENU STAFF                         ")
 		fmt.Println("--------------------------------------------------")
 		fmt.Println("|| 1. Absen Staff                               ||")
+		//Fungsi Absen()
 		fmt.Println("|| 2. Lihat Profile Staff Anda                  ||")
+		//Fungsi Profile()
 		fmt.Println(" ")
 		var b int
 		fmt.Print("Input Pilihan Anda :")
@@ -36,11 +49,14 @@ func main() {
 		fmt.Println("               MENU HRD                           ")
 		fmt.Println("--------------------------------------------------")
 		fmt.Println("|| 1. Manajemen Employee                        ||")
+		//FUNGSI CRUD
 		fmt.Println("|| 2. Print Laporan Employee                    ||")
+		//FUNGSI LAPORAN
 		fmt.Println(" ")
 		var c int
 		fmt.Print("Input Pilihan Anda :")
 		fmt.Scanln(&c)
+		buatEmployee()
 	default:
 		fmt.Println("Mohon input sesuai Menu ")
 		fmt.Println(" ")
@@ -48,6 +64,66 @@ func main() {
 
 	}
 }
+
+func buatEmployee() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	emp := employee.NewManageEmpClient(conn)
+
+	r, err := emp.CreateEmployee(context.Background(), &employee.Employee{
+		Id: 123,
+		Absen:              12,
+		Cuti:               12,
+		Nama:               "staff",
+		Username:           "staff",
+		Password:           "1234",
+		GajiPokok:          120000,
+		TotalGaji:          120000,
+		TunjanganMakan:     200000,
+		TunjanganTransport: 200000,
+		Status:             "MASUK",
+		Role:               "STAFF",
+	})
+	if err != nil {
+		log.Fatalf("Tidak bisa parkir : %v", err)
+	}
+
+	if r.Id == 0 {
+		fmt.Println("Tidak ada Karyawan")
+	} else {
+		fmt.Println("-------------")
+		fmt.Printf("Id : %d\n",r.Id)
+		fmt.Printf("Absen : %d\n",r.Absen)
+		fmt.Printf("Cuti : %d\n",r.Cuti)
+		fmt.Printf("Nama : %s\n",r.Nama)
+		fmt.Printf("Username : %s\n",r.Username)
+		fmt.Printf("Role : %s\n",r.Role)
+	}
+}
+
+func bacaEmployee() {
+	//conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	//if err != nil {
+	//	log.Fatalf("did not connect: %v", err)
+	//}
+	//defer conn.Close()
+	//
+	//emp := employee.NewManageEmpClient(conn)
+	//
+	//r, err := emp.ReadEmployee(context.Background(), &employee.NameId{
+	//	Id:   "1234",
+	//	Name: "staff",
+	//})
+}
+
+// FUNGSI ABSEN
+// Koneksi ke GRPC
+//
+
 
 //TIARA
 
--- server/main.go
+++ server/main.go
@@ -32,6 +32,14 @@ type Cemployee struct {
 	role               string
 }
 
+type empNew struct {
+	empemp *employee.Employee
+}
+
+type empNewList struct {
+	*employee.AllEmployee
+}
+
 var employeeList = []Cemployee{
 	{
 		id:                 1,
@@ -51,7 +59,7 @@ var employeeList = []Cemployee{
 
 func (s *server) CreateEmployee(ctx context.Context, in *employee.Employee) (*employee.Employee, error) {
 	newEmployee := Cemployee{
-		id:                 in.Id,
+		id:                 int64(len(employeeList)+1),
 		absen:              in.Absen,
 		cuti:               in.Cuti,
 		nama:               in.Nama,
@@ -82,6 +90,79 @@ func (s *server) CreateEmployee(ctx context.Context, in *employee.Employee) (*em
 	}, nil
 }
 
+func (s *server) ReadEmployee(ctx context.Context, in *employee.NameId) (*employee.Employee, error) {
+	flag := false
+	index := 0
+	for i:= 0; i<len(employeeList); i++ {
+		if employeeList[i].nama == in.Name {
+			index = i
+			flag = true
+			break
+		}
+	}
+
+	if flag {
+		return &employee.Employee{
+			Id:                 employeeList[index].id,
+			Absen:              employeeList[index].absen,
+			Cuti:               employeeList[index].cuti,
+			Nama:               employeeList[index].nama,
+			Username:           employeeList[index].username,
+			Password:           employeeList[index].password,
+			GajiPokok:          employeeList[index].gajiPokok,
+			TotalGaji:          employeeList[index].totalGaji,
+			TunjanganMakan:     employeeList[index].tunjanganMakan,
+			TunjanganTransport: employeeList[index].tunjanganTransport,
+			Status:             employeeList[index].status,
+			Role:               employeeList[index].role,
+		},nil
+	} else {
+		return nil,nil
+	}
+}
+
+func (s *server) UpdateEmployee(ctx context.Context, in *employee.Employee) (*employee.Employee, error) {
+	return &employee.Employee{
+		Id:                 in.Id,
+		Absen:              in.Absen,
+		Cuti:               in.Cuti,
+		Nama:               in.Nama,
+		Username:           in.Username,
+		Password:           in.Password,
+		GajiPokok:          in.GajiPokok,
+		TotalGaji:          in.TotalGaji,
+		TunjanganMakan:     in.TunjanganMakan,
+		TunjanganTransport: in.TunjanganTransport,
+		Status:             in.Status,
+		Role:               in.Role,
+	},nil
+}
+
+func (s *server) DeleteEmployee(ctx context.Context, in *employee.NameId) (*employee.Result, error) {
+	flag := false
+
+	for i:= 0; i<len(employeeList); i++ {
+		if employeeList[i].nama == in.Name {
+			employeeList = append(employeeList[:i], employeeList[i+1:]...)
+			flag = true
+			break
+		}
+	}
+
+	if flag {
+		return &employee.Result{Response: "BERHASIl"},nil
+	} else {
+		return &employee.Result{Response: "GAGAL"},nil
+	}
+}
+
+//FUNC ABSEN(ctx, in *user) (*employee.Employee) {
+// for _,emps {
+//		if username=emps.id{
+//			returns ---
+//		}
+//}}
+//
 func (s *server) LaporanByUsername(ctx context.Context, input *employee.GetEmpByUsername) (*employee.LaporanEmployee, error) {
 
 	EmployeeByUsername := &employee.LaporanEmployee{
@@ -95,6 +176,7 @@ func (s *server) LaporanByUsername(ctx context.Context, input *employee.GetEmpBy
 
 func main() {
 	lis, err := net.Listen("tcp", port)
+	log.Println("Serv...")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
